0678b2fae1e6061138702de75312c681f29242d6
Alex Schofield <git@ajschof.me>
2025-11-18 04:41:25 +0000

    feat: add info & debug logging to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
 use std::net::UdpSocket;
+use log::{info, debug, LevelFilter};
+use simplelog::{CombinedLogger, Config, TermLogger, TerminalMode, ColorChoice};
 
 fn main() -> std::io::Result<()> {
+    let log_level = if cfg!(debug_assertions) {
+        LevelFilter::Debug
+    } else {
+        LevelFilter::Info
+    };
+
+    CombinedLogger::init(
+        vec![
+            TermLogger::new(log_level, Config::default(),
+                            TerminalMode::Mixed, ColorChoice::Auto),
+        ]
+    ).unwrap();
+
     // First, bind to 0.0.0.0:53535 (UDP)
     // CHANGEME Change to 0.0.0.0 and port 53 when further in development
     let socket = UdpSocket::bind("0.0.0.0:53535")?;
-    println!("Listening on 0.0.0.0:53535");
+    info!("Listening on {}", socket.local_addr()?);
 
     loop {
         // Create a zero-filled 512-byte buffer on every iteration - this is
@@ -19,7 +34,7 @@ fn main() -> std::io::Result<()> {
         // a tuple: size -> number of bytes client sent; src -> client socket
         // address (IP & port)
         let (size, src) = socket.recv_from(&mut buf)?;
-        println!("Received {} bytes from {}", size, src);
+        debug!("Received {} bytes from {}", size, src);
         // For testing with dig command, trim buf to length of what client
         // sent and return to sender
         let response = &buf[..size];
